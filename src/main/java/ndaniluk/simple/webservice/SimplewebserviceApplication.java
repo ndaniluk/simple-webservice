59bc6f6d0449cf5a63291a5f459d94a1121ca7d4
Norbert Daniluk <norbert.daniluk@gmail.com>
2018-05-25 22:24:20 +0200

    Initial commit

---
+++ src/main/java/ndaniluk/simple/webservice/SimplewebserviceApplication.java
@@ -0,0 +1,12 @@
+package ndaniluk.simple.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplewebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplewebserviceApplication.class, args);
+    }
+}
