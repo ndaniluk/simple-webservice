47de946dc949ab4178c4d2a25e01d6169247e803
Norbert Daniluk <norbert.daniluk@gmail.com>
2018-05-26 19:16:38 +0200

    Added methods to EmployeeService class

--- src/main/java/ndaniluk/simple/webservice/employee/services/EmployeeService.java
+++ src/main/java/ndaniluk/simple/webservice/employee/services/EmployeeService.java
@@ -4,16 +4,39 @@ import ndaniluk.simple.webservice.employee.Employee;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Component
 public class EmployeeService {
 
     private static List<Employee> employees = new ArrayList<>();
+    private static int id = 0;
 
 
     public List<Employee> findAll(){
         return employees;
+    }
+
+    public Employee findById(int id){
+        if (employees.get(id) == null)
+            return null;
+        else
+            return employees.get(id);
+    }
+
+    public void add(String name, String surname, String phoneNumber){
+        employees.add(new Employee(++id, name, surname, new Date(), phoneNumber));
+    }
+
+    public Employee remove(int id){
+        if(employees.get(id) == null)
+            return null;
+        else {
+            employees.remove(id);
+            return employees.get(id);
+        }
+
     }
 
 }
